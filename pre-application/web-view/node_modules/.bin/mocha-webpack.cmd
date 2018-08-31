a84568bbbe2f3511edc0a0f7397b2890d14f1ebc
sixliu <359852326@qq.com>
2018-08-31 09:46:38 +0800

    删除前端工厂文件

--- pre-application/web-view/node_modules/.bin/mocha-webpack.cmd
+++
@@ -1,7 +0,0 @@
-@IF EXIST "%~dp0\node.exe" (
-  "%~dp0\node.exe"  "%~dp0\..\mocha-webpack\bin\mocha-webpack" %*
-) ELSE (
-  @SETLOCAL
-  @SET PATHEXT=%PATHEXT:;.JS;=;%
-  node  "%~dp0\..\mocha-webpack\bin\mocha-webpack" %*
-)
